827303e218076c5c897e8ff9fc756e151f68f980
Nilakshi <nilaxi2102@gmail.com>
2025-09-02 23:19:52 +0530

    Time: 5 ms (100.00%) | Memory: 44.4 MB (91.04%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        // Sort by x ascending, then y descending
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            return b[1] - a[1];
+        });
+
+        int n = points.length;
+        int ans = 0;
+
+       
+        for (int i = 0; i < n; i++) {
+            int topY = points[i][1];
+            int botY = Integer.MIN_VALUE;  
+
+          
+            for (int j = i + 1; j < n; j++) {
+                int yj = points[j][1];
+                
+                if (topY >= yj && yj > botY) {
+                    ans++;
+                    botY = yj;  
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
